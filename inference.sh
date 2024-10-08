2794aa76216947f9d8094958240ffd11f1c28c12
Sebastian Weninger <sebastian.weninger@gmail.com>
2024-10-08 14:56:13 +0200

    ADDED inference and train shell commands

--- inference.sh
+++ inference.sh
@@ -3,3 +3,9 @@ python main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume ./output/check
 
  python main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume /media/sebastian/TEMP/poet/poet_ycbv_yolo.pth --inference --inference_path /media/sebastian/TEMP/poet/test --inference_output ./output_inference/ \
  --backbone dinoyolo --backbone_cfg ./configs/ycbv_yolov4-csp.cfg --backbone_weights /media/sebastian/TEMP/poet/ycbv_yolo_weights.pt --dataset_path /media/sebastian/TEMP/poet/datasets/ycbv --class_info /annotations/ycbv_classes.json --lr_backbone 0.0
+
+######################################################################################################
+
+ pythn main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume /home/sebastian/repos/poet/train/2024-10-02_18:21:04/checkpoint.pth --inference --inference_path /home/sebastian/repos/master_project/dataset/records/cabinet_1/rgb \
+ --inference_output inf/dino_yolo_custom/ --backbone dinoyolo --backbone_cfg ./configs/ycbv_yolov4-csp.cfg --backbone_weights /media/sebastian/TEMP/poet/ycbv_yolo_weights.pt --lr_backbone 0.0 --dataset_path /media/sebastian/TEMP/poet/datasets/custom \
+ --class_info /annotations/custom_classes.json --model_symmetry /annotations/custom_symmetries.json --dataset custom --rgb_augmentation --grayscale --class_mode agnostic
--- train.sh
+++ train.sh
@@ -22,4 +22,11 @@ python main.py --eval_batch_size 32 --batch_size 32 --enc_layers 5 --dec_layers 
 # Train PoET from scratch on YOLO backbone # ~13s/it
  python main.py --enc_layers 5 --dec_layers 5 --lr 0.00002 --nheads 16 --backbone yolov4 --lr_backbone 0.0 --backbone_cfg ./configs/ycbv_yolov4-csp.cfg \
  --backbone_weights /media/sebastian/TEMP/poet/ycbv_yolo_weights.pt --dataset_path  /media/sebastian/TEMP/poet/datasets/ycbv  --class_info /annotations/ycbv_classes.json \
- --model_symmetry /annotations/ycbv_symmetries.json --train_set train_pbr --output_dir output/ --bbox_mode backbone --epochs 50 --translation_loss_coef 2 
+ --model_symmetry /annotations/ycbv_symmetries.json --train_set train_pbr --output_dir output/ --bbox_mode backbone --epochs 50 --translation_loss_coef 2
+
+##############################################################
+
+# Train PoET from scratch on DINO_YOLO backbone # ~13s/it
+ python main.py --enc_layers 5 --dec_layers 5 --nheads 16 --batch_size 16 --eval_batch_size 16 --n_classes 16 --class_mode agnostic --backbone dinoyolo --lr_backbone 0.0 --backbone_cfg ./configs/ycbv_yolov4-csp.cfg --backbone_weights /media/sebastian/TEMP/poet/ycbv_yolo_weights.pt \
+ --dataset_path /media/sebastian/TEMP/poet/datasets/custom --class_info /annotations/custom_classes.json --model_symmetry /annotations/custom_symmetries.json --train_set train --eval_set val --test_set test --eval_interval 1 \
+ --output_dir train/ --bbox_mode backbone --dataset custom --grayscale --rgb_augmentation --translation_loss_coef 2.0 --rotation_loss_coef 1.0 --epochs 50 --lr 0.000035 --lr_drop 50 --gamma 0.1
