9b72d2769f18f609a0c66f6a798c88c2f0c60c2a
Sebastian Weninger <sebastian.weninger@gmail.com>
2024-11-04 16:48:49 +0100

    ADDED dronespace inference command

--- inference.sh
+++ inference.sh
@@ -9,3 +9,9 @@ python main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume ./output/check
  pythn main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume /home/sebastian/repos/poet/train/2024-10-02_18:21:04/checkpoint.pth --inference --inference_path /home/sebastian/repos/master_project/dataset/records/cabinet_1/rgb \
  --inference_output inf/dino_yolo_custom/ --backbone dinoyolo --backbone_cfg ./configs/ycbv_yolov4-csp.cfg --backbone_weights /media/sebastian/TEMP/poet/ycbv_yolo_weights.pt --lr_backbone 0.0 --dataset_path /media/sebastian/TEMP/poet/datasets/custom \
  --class_info /annotations/custom_classes.json --model_symmetry /annotations/custom_symmetries.json --dataset custom --rgb_augmentation --grayscale --class_mode agnostic
+
+
+######## DRONESPACE
+python main.py --enc_layers 5 --dec_layers 5 --nheads 16 --resume /home/wngicg/Desktop/repos/poet/results/train/2024-10-06_12_31_12/checkpoint.pth --inference --inference_path /home/wngicg/Desktop/repos/datasets/custom/val/cabinet_1/rgb/ \
+--inference_output inf/dino_yolo_custom/ --backbone dinoyolo --backbone_cfg ./configs/ycbv_yolov4-csp.cfg --backbone_weights /home/wngicg/Desktop/repos/ycbv_yolo_weights.pt --lr_backbone 0.0 --dataset_path /home/wngicg/Desktop/repos/datasets/custom \
+--class_info /annotations/custom_classes.json --model_symmetry /annotations/custom_symmetries.json --dataset custom --rgb_augmentation --grayscale --class_mode agnostic
