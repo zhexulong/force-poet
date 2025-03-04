d315e0eaeb0c94f0d99ff8d0a7fc5ca142821312
Sebastian Weninger <sebastian.weninger@gmail.com>
2025-03-04 15:38:52 +0100

    REPLACED YOLO for DINO with origin PoET YOLO!!!

--- models/backbone_yolo_dino.py
+++ models/backbone_yolo_dino.py
@@ -27,6 +27,8 @@ from .groundingdino.models.GroundingDINO import build_groundingdino
 from .groundingdino.util.misc import clean_state_dict
 import models.groundingdino.datasets.transforms as T
 
+from .cns_yolo import build_cns_yolo
+
 from util.misc import NestedTensor
 from util import logger
 
@@ -34,97 +36,97 @@ import supervision as sv
 import cv2
 
 
-class YOLOBackbone(Darknet):
-    """
-    YOLOv4 Object Detector.
-    Returns detected/predicted objects (class, bounding box) and the feature maps.
-    """
-    def __init__(self, args, train_backbone: bool, return_interm_layers: bool, return_layers=[142, 157, 172]):
-
-        # TODO: Check whether Imagesize argument is needed
-        super().__init__(args.backbone_cfg)
-
-        self.return_iterm_layers = return_interm_layers
-
-        if return_interm_layers:
-            self.return_layers = {str(i): v for i, v in enumerate(return_layers)}
-            self.num_channels = [x[0].out_channels for x in [self.module_list[i] for i in return_layers]]
-            self.strides = [x[0].stride[0] for x in [self.module_list[i] for i in return_layers]]
-        else:
-            self.return_layers = {'0': 172}
-            self.num_channels = self.module_list[return_layers[0]].out_channels
-            self.strides = self.module_list[return_layers[0]].strides
-
-        # Set threshold parameters
-        self.conf_thres = args.backbone_conf_thresh
-        self.iou_thres = args.backbone_iou_thresh
-        self.agnostic_nms = args.backbone_agnostic_nms
-
-        # Freeze backbone if it should not be trained
-        self.train_backbone = train_backbone
-        if not train_backbone:
-            for name, parameter in self.named_parameters():
-                parameter.requires_grad_(False)
-
-    def forward_backbone(self, x, verbose=False):
-        yolo_out, out = [], []
-        intermediate = OrderedDict()
-        intermediate_i = 0
-        if verbose:
-            print('0', x.shape)
-            str_o = ''
-
-        # Passing the image through the YOLO model layer by layer
-        for i, module in enumerate(self.module_list):
-            name = module.__class__.__name__
-            if name in ['WeightedFeatureFusion', 'FeatureConcat', 'FeatureConcat2', 'FeatureConcat3',
-                        'FeatureConcat_l']:  # sum, concat
-                if verbose:
-                    l = [i - 1] + module.layers  # layers
-                    sh = [list(x.shape)] + [list(out[i].shape) for i in module.layers]  # shapes
-                    str_o = ' >> ' + ' + '.join(['layer %g %s' % x for x in zip(l, sh)])
-                x = module(x, out)  # WeightedFeatureFusion(), FeatureConcat()
-            elif name == 'YOLOLayer':
-                yolo_out.append(module(x, out))
-            else:  # run module directly, i.e. mtype = 'convolutional', 'upsample', 'maxpool', 'batchnorm2d' etc.
-                x = module(x)
-
-            if i in self.return_layers.values():
-                intermediate[str(intermediate_i)] = x
-                intermediate_i += 1
-
-            out.append(x if self.routs[i] else [])
-            if verbose:
-                print('%g/%g %s -' % (i, len(self.module_list), name), list(x.shape), str_o)
-                str_o = ''
-
-        if self.training:
-            # TODO: Write code when backbone is not frozen
-            # We want to return the same as the original yolo, but also the predicted outputs as we need them for further processing
-            raise NotImplementedError
-        else:
-            x, p = zip(*yolo_out)  # inference output, training output
-            x = torch.cat(x, 1)  # cat yolo outputs
-            # Determine prediction from yolo output layers: pred = [bbox (4), conf, class]
-            pred = non_max_suppression(x, self.conf_thres, self.iou_thres, classes=None,
-                                       agnostic=self.agnostic_nms)
-        return pred, intermediate
-
-    def forward_once(self, tensor_list, augment=False, verbose=False):
-        # Pass Image through YOLO
-        predictions, xs = self.forward_backbone(tensor_list.tensors)
-        # Adjust predicted classes by 1 as class 0 is "background / dummy" in PoET
-        for prediction in predictions:
-            if prediction is not None:
-                prediction[:, 5] += 1
-
-        out: Dict[str, NestedTensor] = {}
-        for name, x in xs.items():
-            m = tensor_list.mask
-            assert m is not None
-            mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
-            out[name] = NestedTensor(x, mask)
-        return predictions, out
+# class YOLOBackbone(Darknet):
+#     """
+#     YOLOv4 Object Detector.
+#     Returns detected/predicted objects (class, bounding box) and the feature maps.
+#     """
+#     def __init__(self, args, train_backbone: bool, return_interm_layers: bool, return_layers=[142, 157, 172]):
+#
+#         # TODO: Check whether Imagesize argument is needed
+#         super().__init__(args.backbone_cfg)
+#
+#         self.return_iterm_layers = return_interm_layers
+#
+#         if return_interm_layers:
+#             self.return_layers = {str(i): v for i, v in enumerate(return_layers)}
+#             self.num_channels = [x[0].out_channels for x in [self.module_list[i] for i in return_layers]]
+#             self.strides = [x[0].stride[0] for x in [self.module_list[i] for i in return_layers]]
+#         else:
+#             self.return_layers = {'0': 172}
+#             self.num_channels = self.module_list[return_layers[0]].out_channels
+#             self.strides = self.module_list[return_layers[0]].strides
+#
+#         # Set threshold parameters
+#         self.conf_thres = args.backbone_conf_thresh
+#         self.iou_thres = args.backbone_iou_thresh
+#         self.agnostic_nms = args.backbone_agnostic_nms
+#
+#         # Freeze backbone if it should not be trained
+#         self.train_backbone = train_backbone
+#         if not train_backbone:
+#             for name, parameter in self.named_parameters():
+#                 parameter.requires_grad_(False)
+#
+#     def forward_backbone(self, x, verbose=False):
+#         yolo_out, out = [], []
+#         intermediate = OrderedDict()
+#         intermediate_i = 0
+#         if verbose:
+#             print('0', x.shape)
+#             str_o = ''
+#
+#         # Passing the image through the YOLO model layer by layer
+#         for i, module in enumerate(self.module_list):
+#             name = module.__class__.__name__
+#             if name in ['WeightedFeatureFusion', 'FeatureConcat', 'FeatureConcat2', 'FeatureConcat3',
+#                         'FeatureConcat_l']:  # sum, concat
+#                 if verbose:
+#                     l = [i - 1] + module.layers  # layers
+#                     sh = [list(x.shape)] + [list(out[i].shape) for i in module.layers]  # shapes
+#                     str_o = ' >> ' + ' + '.join(['layer %g %s' % x for x in zip(l, sh)])
+#                 x = module(x, out)  # WeightedFeatureFusion(), FeatureConcat()
+#             elif name == 'YOLOLayer':
+#                 yolo_out.append(module(x, out))
+#             else:  # run module directly, i.e. mtype = 'convolutional', 'upsample', 'maxpool', 'batchnorm2d' etc.
+#                 x = module(x)
+#
+#             if i in self.return_layers.values():
+#                 intermediate[str(intermediate_i)] = x
+#                 intermediate_i += 1
+#
+#             out.append(x if self.routs[i] else [])
+#             if verbose:
+#                 print('%g/%g %s -' % (i, len(self.module_list), name), list(x.shape), str_o)
+#                 str_o = ''
+#
+#         if self.training:
+#             # TODO: Write code when backbone is not frozen
+#             # We want to return the same as the original yolo, but also the predicted outputs as we need them for further processing
+#             raise NotImplementedError
+#         else:
+#             x, p = zip(*yolo_out)  # inference output, training output
+#             x = torch.cat(x, 1)  # cat yolo outputs
+#             # Determine prediction from yolo output layers: pred = [bbox (4), conf, class]
+#             pred = non_max_suppression(x, self.conf_thres, self.iou_thres, classes=None,
+#                                        agnostic=self.agnostic_nms)
+#         return pred, intermediate
+#
+#     def forward_once(self, tensor_list, augment=False, verbose=False):
+#         # Pass Image through YOLO
+#         predictions, xs = self.forward_backbone(tensor_list.tensors)
+#         # Adjust predicted classes by 1 as class 0 is "background / dummy" in PoET
+#         for prediction in predictions:
+#             if prediction is not None:
+#                 prediction[:, 5] += 1
+#
+#         out: Dict[str, NestedTensor] = {}
+#         for name, x in xs.items():
+#             m = tensor_list.mask
+#             assert m is not None
+#             mask = F.interpolate(m[None].float(), size=x.shape[-2:]).to(torch.bool)[0]
+#             out[name] = NestedTensor(x, mask)
+#         return predictions, out
 
 
 class YOLODINOBackbone(nn.Module):
@@ -530,7 +532,6 @@ class YOLODINOBackbone(nn.Module):
             pred = torch.hstack((box, logits, torch.tensor(-1).to("cuda")))
           p.append(pred)
 
-        # If all predictions are below label matching threshold
         if len(p) != 0:
           p = torch.stack(p)
           predictions.append(p)
@@ -550,15 +551,7 @@ def build_yolo_dino(args):
     dino.load_state_dict(clean_state_dict(checkpoint["model"]), strict=False)
     dino.eval()
 
-    train_backbone = args.lr_backbone > 0
-    return_interm_layers = (args.num_feature_levels > 1)
-    cns_yolo = YOLOBackbone(args, train_backbone, return_interm_layers)
-    if args.backbone_weights is not None:
-        try:
-            cns_yolo.load_state_dict(torch.load(args.backbone_weights, map_location=select_device())['model'])
-        except Exception as e:
-            print(e)
-            load_darknet_weights(cns_yolo, args.backbone_weights)
+    cns_yolo = build_cns_yolo(args)
 
     yolo_dino = YOLODINOBackbone(yolo_backbone=cns_yolo, dino_backbone=dino, args=args)
     return yolo_dino
